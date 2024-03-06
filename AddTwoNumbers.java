c22ef409f426573c9d86e67b7d0c800acbb5f9b7
Naeem110 <154973993+Naeem110@users.noreply.github.com>
2024-03-06 23:02:05 +0500

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,13 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Define two numbers to be added
+        int number1 = 5;
+        int number2 = 7;
+
+        // Calculate the sum
+        int sum = number1 + number2;
+
+        // Display the result
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
